7f710571679e15b10119d44d2f52683053c39b25
Raptor56 <30021338+Raptor56@users.noreply.github.com>
2017-09-11 00:37:32 +0900

    一応完成 vpythonで表示時に開店の中心（座標原点）オブジェクトの中心にする必要あり

---
+++ Minecraft_csv-viewer/Minecraft_csv_viewer.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import vpython as vs
+import numpy as np
+
+
+# CSVファイルを読み込み、リスト化
+
+def open_with_numpy_loadtxt_2(filename):
+    with open(filename, 'r') as file:
+        line = file.readline()
+        header = line.strip().split(',')
+        #data = np.loadtxt(file, delimiter=',')
+        data = np.genfromtxt( filename, delimiter=",",skip_header=1, filling_values=(0, 0, 0) )
+    return map(int,header), data
+
+header, data = open_with_numpy_loadtxt_2('copy.csv')
+#header, data = open_with_numpy_loadtxt_2(input("File Name = "))
+
+x, z, y = header
+print("x,z,y = ",x,z,y)
+
+List = data.tolist()
+
+PList = [[[[0 for n in range(2)] for i in range(x)] for j in range(z)] for k in range(y)]
+
+
+m = 0
+for k in range(y):
+    for j in range(z):
+        for i in range(x):
+            PList[k][j][i] = [List[m][0], List[m][1]]
+
+            if List[m][0] != 0:
+
+                vs.box(pos=vs.vector(i-0.5,k-0.5,j-0.5),length=1, width=1, height=1,color=vs.color.green)
+
+            m += 1
+            print(PList[k][j][i],end = "")
+        print()
+    print()
