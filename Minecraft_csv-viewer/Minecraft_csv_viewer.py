6c4a90dba7dbe126d793fbbfda0700c79d1355a9
Raptor56 <30021338+Raptor56@users.noreply.github.com>
2017-09-11 23:20:28 +0900

    回転軸中心をオブジェクトの中心に変更 不要なソースをコメント化

--- Minecraft_csv-viewer/Minecraft_csv_viewer.py
+++ Minecraft_csv-viewer/Minecraft_csv_viewer.py
@@ -11,7 +11,6 @@ def open_with_numpy_loadtxt_2(filename):
     with open(filename, 'r') as file:
         line = file.readline()
         header = line.strip().split(',')
-        #data = np.loadtxt(file, delimiter=',')
         data = np.genfromtxt( filename, delimiter=",",skip_header=1, filling_values=(0, 0, 0) )
     return map(int,header), data
 
@@ -23,20 +22,21 @@ print("x,z,y = ",x,z,y)
 
 List = data.tolist()
 
-PList = [[[[0 for n in range(2)] for i in range(x)] for j in range(z)] for k in range(y)]
+#PList = [[[[0 for n in range(2)] for i in range(x)] for j in range(z)] for k in range(y)]
 
 
 m = 0
 for k in range(y):
     for j in range(z):
         for i in range(x):
-            PList[k][j][i] = [List[m][0], List[m][1]]
+            #PList[k][j][i] = [List[m][0], List[m][1]]
 
             if List[m][0] != 0:
 
-                vs.box(pos=vs.vector(i-0.5,k-0.5,j-0.5),length=1, width=1, height=1,color=vs.color.green)
+                vs.box(pos=vs.vector(i - 0.5 - x / 2, k - 0.5 - y / 2, j - 0.5 - z / 2),length=1, width=1, height=1,color=vs.color.green)
 
+            print([List[m][0], List[m][1]],end = "")
             m += 1
-            print(PList[k][j][i],end = "")
+    #        print(PList[k][j][i],end = "")
         print()
     print()
